7c18652937e156de4ec10d567c516cfd9bee5b13
Taras1958 <118650875+Taras1958@users.noreply.github.com>
2023-01-30 09:06:33 +0300

    Finish Commit

--- app/src/main/java/ru/stambul4you/recyclerviewitdev/MainActivity.kt
+++ app/src/main/java/ru/stambul4you/recyclerviewitdev/MainActivity.kt
@@ -2,13 +2,50 @@ package ru.stambul4you.recyclerviewitdev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import ru.stambul4you.recyclerviewitdev.adapter.UserAdapter
 import ru.stambul4you.recyclerviewitdev.databinding.ActivityMainBinding
+import ru.stambul4you.recyclerviewitdev.model.UserModel
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var adapter: UserAdapter
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initial()
     }
+
+    private fun initial() {
+        recyclerView = binding.rvUser
+        adapter = UserAdapter(this)
+        recyclerView.adapter = adapter
+        adapter.setList(myUser())
+    }
+
+    fun myUser(): ArrayList<UserModel>{
+        val userList = ArrayList<UserModel>()
+
+        val user = UserModel("Petrov","Vasya")
+        userList.add(user)
+
+        val user2 = UserModel("Ivanov","Petr")
+        userList.add(user2)
+
+        val user3 = UserModel("Sidorov","Nikolay")
+        userList.add(user3)
+
+        val user4 = UserModel("Nikonov","Anton")
+        userList.add(user4)
+
+
+
+        return userList
+
+    }
+
 }
--- app/src/main/java/ru/stambul4you/recyclerviewitdev/adapter/UserAdapter.kt
+++ app/src/main/java/ru/stambul4you/recyclerviewitdev/adapter/UserAdapter.kt
@@ -1,16 +1,19 @@
 package ru.stambul4you.recyclerviewitdev.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_user_layout.view.*
 import ru.stambul4you.recyclerviewitdev.R
 import ru.stambul4you.recyclerviewitdev.model.UserModel
 
-class UserAdapter: RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+class UserAdapter(private val context: Context): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
     private var userList = emptyList<UserModel>()
+
 
 
     class UserViewHolder(view: View): RecyclerView.ViewHolder(view)
@@ -23,6 +26,10 @@ class UserAdapter: RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         holder.itemView.tv_last_name.text = userList[position].lastName
         holder.itemView.tv_first_name.text = userList[position].firstName
+        
+        holder.itemView.setOnClickListener {
+            Toast.makeText(context, "$position", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int {
